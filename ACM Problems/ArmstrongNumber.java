8a51a59410b2789cc0edc0e212221fc9fdbc4ebf
Cirjon Dizon <cirjondizon@gmail.com>
2016-07-31 20:33:16 +0800

    ArmstrongNumber
    Our ACM Problem for the week.

---
+++ ACM Problems/ArmstrongNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class ArmstrongNumber 
+{
+    public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of test cases: ");
+        int T = s.nextInt();
+        System.out.print("Enter the three digit case numbers: ");
+        int[] N = new int[T];
+        for (int y = 0; y<T; y++)
+            N[y] = s.nextInt();
+        int ones, tens, hundreds, temp, temp2;
+        for(int x=0; x<T; x++)
+        {
+            
+            ones = N[x]%10;
+            temp = N[x]/10;
+            tens = temp%10;
+            hundreds = temp/10;
+            if(N[x] == (Math.pow(ones,3) + Math.pow(tens, 3) + Math.pow(hundreds, 3)))
+                System.out.println(N[x] + " is an Armstrong Number");
+            else
+                System.out.println(N[x] + " is not an Armstrong Number");
+        }
+        
+    }
+    
+}
