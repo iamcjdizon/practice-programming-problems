4627b2fd5b1eeee1ecbd7c2e3ee5c42596858b33
Cirjon Dizon <cirjondizon@gmail.com>
2016-07-31 20:33:37 +0800

    Watermelon
    Our ACM Problem for the week.

---
+++ ACM Problems/Watermelon.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Watermelon 
+{
+    public static void main(String[] args) 
+	{
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter weight of watermelon in Kilos: ");
+        int w = s.nextInt();
+        if(1 <= w &&  w<=100)
+        {
+            if(w%2==0)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+        }
+        else
+            System.out.println("INVALID!");
+    }
+    
+}
